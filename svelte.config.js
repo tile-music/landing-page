bc966301f3b2c8258b6d534d09edb7d2e4e3c970
Ivy Bixler <ivybixler@gmail.com>
2025-12-28 15:11:41 -0500

    edited config to make github pages deploy from cmd line

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,18 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+
+const repoName = 'your-repo-name';
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: "index.html"
+    })
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [svelte()],
+    build: {
+        outDir: "build"
+    }
 })
